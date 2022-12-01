704043c7ad7d9b895271a5205604490351e40d1b
Arfushs <canberk.soydan@hotmail.com>
2022-12-01 21:04:53 +0300

    Cloud Spawner

---
+++ DinoGame/Scripts/Cloud.gd
@@ -0,0 +1,17 @@
+extends Sprite
+
+
+var speed : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	
+	position.x -= speed
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
---
+++ DinoGame/Scripts/CloudSpawner.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+
+export (PackedScene) var clouds
+
+
+
+func _ready():
+	randomize()
+	
+
+func _on_CloudTimer_timeout():
+	
+	var cloud = clouds.instance()
+	var cloud_pos_x = 1200
+	var cloud_pos_y = randi() % 321 + 10
+	var cloud_scale = rand_range(3,4)
+	var cloud_flip_h = randi() % 2 == 0 ## True or False
+	var cloud_speed = rand_range(1.5,2)  
+	var cloud_frame = randi() % 7
+	
+	cloud.position.x = cloud_pos_x
+	cloud.position.y = cloud_pos_y
+	cloud.scale.x = cloud_scale
+	cloud.scale.y = cloud_scale
+	cloud.flip_h = cloud_flip_h
+	cloud.speed = cloud_speed
+	cloud.frame = cloud_frame
+	
+	add_child(cloud)
+	
+
