b9dae84c492a7219b9f874a0861f6308c3dcf104
Arfushs <canberk.soydan@hotmail.com>
2022-12-02 17:35:28 +0300

    Final

--- DinoGame/Scripts/Score.gd
+++ DinoGame/Scripts/Score.gd
@@ -7,12 +7,12 @@ var score = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.text = "Score: " + str(score)
+	self.text = "SCORE: " + str(score)
 	
 func _physics_process(delta):
 	if(GlobalScript.engine_speed !=0):
 		score += 0.3 * GlobalScript.score_mult
-		self.text = "Score: " + str(int(score))
+		self.text = "SCORE: " + str(int(score))
 	if(int(score) % 200 == 0 && int(score)>=200):
 		GlobalScript.engine_speed += 0.2
 		GlobalScript.score_mult += 0.01
